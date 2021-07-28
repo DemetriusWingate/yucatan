a24561b3f8fc70903d54a8392135a196c85c7c1f
Demetrius Wingate <dwingate1991@gmail.com>
2021-07-28 17:08:04 -0400

    fixed javascript to validate form and fixed landing image

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
+    /* This function switches the nav menus */
+    function myNav () {
+      var x=document.getElementById("navLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+
+
+/*this function validates the form*/
+
 function myFunction () {
     var x=document.getElementById('name').value;
     if (x == "") {
     alert ("You didn't tell us your name!");
         return false;
      } else {
-    alert ("Thanks, " +x + " for signing up for our newsletter!");
+    alert ("Thanks, " + x + " for signing up for our newsletter!");
     }
     }
-
-    /* This function switches the nav menus */
-function myFunction () {
-    var x=document.getElementById("navLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
